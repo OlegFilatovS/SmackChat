33be4e23c607a3166e34fd35c11b9d9cb746f13c
Филатов Олег <joincs@yandex.ru>
2017-11-14 23:47:59 +0300

    Added reveal menu

---
+++ Smack/Supporting Files/SWReveal-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SWRevealBridgingHeader.h
+//  Smack
+//
+//  Created by Филатов Олег on 14.11.17.
+//  Copyright © 2017 OlegDev. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
